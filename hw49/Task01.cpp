0649fd8ca8827351a2e1559071b5c7692227dd02
bgtttttt <vladislav.navitski@gmail.com>
2024-03-24 11:59:41 +0300

    Добавьте файлы проекта.

---
+++ hw49/Task01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class School {
+public:
+	string name;
+	int numOfStudents;
+};
+
+int main() {
+	School school1;
+	cout << "Input name of school:\n";
+	cin >> school1.name;
+	cout << "Input number if students in school:\n";
+	cin >> school1.numOfStudents;
+
+	cout << "\n\nName: " << school1.name << "; Number of students: " 
+		<< school1.numOfStudents << "\n";
+}
